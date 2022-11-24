bf12db6762e533cf30a108e53e8f4d3f18e0e631
Artur Abdullin <artur.abdullin@gmail.com>
2022-11-24 10:27:39 +0300

    feat(server): add routes aggregator

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import cors from "cors";
+import router from "./routes";
 
 const app = express();
 
 // cross-origin resource sharing
 app.use(cors());
+app.use(router);
 
 export default app;
